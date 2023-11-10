2e48100e2505fbf60835d410fcf2953382c44f30
Thiago Leandro Liporace <85422792+thiliporace@users.noreply.github.com>
2023-11-10 14:49:46 -0300

    Initial Commit

---
+++ Sources/AulaPackageThi/AulaPackageThi.swift
@@ -0,0 +1,18 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+
+import SwiftUI
+
+// Lembrar de colocar tudo em public para quem baixar conseguir acessar
+public struct VerdadeView: View {
+    
+    public init(){}
+    
+    public var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    VerdadeView()
+}
